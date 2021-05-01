5f4152e08bc3fde4e14a03464c46f91dae5de81c
YuSan357 <forestgreen14689@gmail.com>
2021-05-01 14:43:24 +0900

    Made dake-theme for theme-switch-button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,42 @@
-$('body').css('background-color','#333');
+$('header .canvas-container .theme-switch-button').click(function(){
 
-$('header .canvas-container .logo').css('color','white');
+  // body
+  $('body').css('background-color','#333');
 
-$('header .canvas-container .global-nav li a').css('color','#aaa');
-$('header .canvas-container .global-nav li a').hover(
-  function(){
-    $(this).css('color','white');
-  },
-  function(){
-    $(this).css('color','#aaa');
-  }
-);
+  // header logo
+  $('header .canvas-container .logo').css('color','white');
 
-$('.section .details-container').css('border-color','#777');
-$('.section .details-container h1').css('color','white');
-$('.section .details-container h2').css('color','white');
+  // global-nav items
+  $('header .canvas-container .global-nav li a').css('color','#aaa');
+
+  // global-nav's hover state 
+  $('header .canvas-container .global-nav li a').hover(
+    function(){
+      $(this).css('color','white');
+    },
+    function(){
+      $(this).css('color','#aaa');
+    }
+  );
+
+  // theme-switch-button's hover state
+  $('header .canvas-container .theme-switch-button').hover(
+    function(){
+      $(this).css('border-color','white');
+      $('header .canvas-container .theme-switch-button i').css('color','white');
+    },
+    function(){
+      $(this).css('border-color','#aaa');
+      $('header .canvas-container .theme-switch-button i').css('color','#aaa');
+    }
+  );
 
 
-$('footer').css('background-color','#222');
+  // main
+  $('.section .details-container').css('border-color','#777');
+  $('.section .details-container h1').css('color','white');
+  $('.section .details-container h2').css('color','white');
+  // footer
+  $('footer').css('background-color','#222');
+
+});
