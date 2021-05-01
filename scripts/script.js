3899ad78f1352e0a6147730eb5ad7d350043fa4b
YuSan357 <forestgreen14689@gmail.com>
2021-05-01 21:35:45 +0900

    Completed making dark theme.

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,24 @@ $(function(){
   
   let isDarkTheme = 0; // 0 : light-theme, 1 : dark-theme.
 
+  let isJustClicked = 0;
+
 
   $('header .canvas-container .theme-switch-button').click(function(){
+
+    isJustClicked = 1;
+    if(isJustClicked){
+      $(this).toggleClass('dark-theme-white');
+      $('header .canvas-container .theme-switch-button i').toggleClass('dark-theme-white');
+      isJustClicked = 0;
+    }
+
+    $(this).mouseout(function () { 
+      
+    });
+
+
+  
 
     // global-navのhover疑似クラスのために、ダークテーマかどうか判定できるようにしておく。
     isDarkTheme = 1 - isDarkTheme;
@@ -13,49 +29,66 @@ $(function(){
     // BODY
     $('body').toggleClass('dark-theme-333');
 
-
     // HEADER
     $('header .canvas-container .logo').toggleClass('dark-theme-white');
     $('header .canvas-container .global-nav li a').toggleClass('dark-theme-gray');
 
-    // global-nav's hover state 
-    $('header .canvas-container .global-nav li a').hover(
-      function(){
-        if(isDarkTheme){
-          $(this).addClass('dark-theme-white')
-        }
-      },
-      function(){
-        $(this).removeClass('dark-theme-white');
-      }
-    );
-    
-    // theme-switch-button's hover state
-    $('header .canvas-container .theme-switch-button').hover(
-      function(){
-        if(isDarkTheme){
-          $(this).addClass('dark-theme-white');
-          $('header .canvas-container .theme-switch-button i').addClass('dark-theme-white');
-        }
-      },
-      function(){
-        $(this).removeClass('dark-theme-white');
-        $('header .canvas-container .theme-switch-button i').removeClass('dark-theme-white');
-      }
-    );
-
-    
     // MAIN
     $('.section .details-container').toggleClass('dark-theme-dimgray');
     $('.section .details-container h1').toggleClass('dark-theme-white');
     $('.section .details-container h2').toggleClass('dark-theme-white');
 
-    
     // FOOTER
     $('footer').toggleClass('dark-theme-222');
 
-
   });
+
+
+  // global-nav's hover state 
+  $('header .canvas-container .global-nav li a').hover(
+    function(){
+      if(isDarkTheme){
+        $(this).addClass('dark-theme-white')
+      }
+    },
+    function(){
+      $(this).removeClass('dark-theme-white');
+    }
+  );
+  
+  // theme-switch-button's hover state
+  if(!isJustClicked){
+
+    $('header .canvas-container .theme-switch-button').hover(
+      function(){
+        if(isDarkTheme){
+        $(this).addClass('dark-theme-white');
+        $('header .canvas-container .theme-switch-button i').addClass('dark-theme-white');
+      }
+    },
+    function(){
+      $(this).removeClass('dark-theme-white');
+      $('header .canvas-container .theme-switch-button i').removeClass('dark-theme-white');
+    }
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
