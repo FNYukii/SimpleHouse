a771782c4a35269d48ff63d4d9eabbd82409e2d8
YuSan357 <forestgreen14689@gmail.com>
2021-05-05 01:12:28 +0900

    ページ読み込み時に前回のテーマが引き継がれるようにした。

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,74 @@
 $(function(){
 
-  // THEME SWITCH
-  let isDarkTheme = 0; // 0 : light-theme, 1 : dark-theme.
-  let isJustSwitched = 0; // テーマ変更直後にtheme-buttonの色を変更するためのフラグ変数
+  // ページ読み込み時にテーマを設定する。
+  console.log("isDarkTheme : " + localStorage.getItem('isDarkTheme'));
+  if(localStorage.getItem('isDarkTheme') == "0"){
+    console.log("light themeで開始");
+  }else{
+    
+    // ダークテーマに切り替える。
+    // BODY
+    $('body').css('transition','0s');
+    $('body').addClass('dark-theme-333');
+    // HEADER
+    $('header .canvas-container .logo').addClass('dark-theme-white');
+    // MAIN
+    $('main .section .details-container').addClass('dark-theme-dimgray');
+    $('main .section .details-container h1').addClass('dark-theme-white');
+    $('main .section .details-container h2').addClass('dark-theme-white');
+    // FOOTER
+    $('footer').addClass('dark-theme-222');
 
+    // ログ出力
+    console.log("dark themeで開始");
+
+  }
+  
+  
+
+  // テーマ切り替え処理
+  let isJustSwitched = 0;
   $('.trigger-to-switch-theme').click(function(){
 
+    // テーマ切り替え直後はマウスオーバー状態なので、theme-buttonを強調表示する。
     isJustSwitched = 1;
-    console.log("isJustSwitched : " + isJustSwitched);
     if(isJustSwitched){
-      $(this).toggleClass('dark-theme-white');
+      $('header .canvas-container .theme-button').toggleClass('dark-theme-white');
       $('header .canvas-container .theme-button i').toggleClass('dark-theme-white');
       isJustSwitched = 0;
-      console.log("isJustSwitched : " + isJustSwitched);
     }
 
-    // global-navのhover疑似クラスのために、ダークテーマかどうか判定できるようにしておく。
-    isDarkTheme = 1 - isDarkTheme;
-    console.log("isDarkTheme : " + isDarkTheme);
-
     // BODY
+    $('body').css('transition','0.8s');
     $('body').toggleClass('dark-theme-333');
-
     // HEADER
     $('header .canvas-container .logo').toggleClass('dark-theme-white');
-
     // MAIN
     $('main .section .details-container').toggleClass('dark-theme-dimgray');
     $('main .section .details-container h1').toggleClass('dark-theme-white');
     $('main .section .details-container h2').toggleClass('dark-theme-white');
-
     // FOOTER
     $('footer').toggleClass('dark-theme-222');
 
+    // localStorageの値を更新する。
+    if(localStorage.getItem('isDarkTheme') == "0"){
+      localStorage.setItem('isDarkTheme',"1");
+    }else{
+      localStorage.setItem('isDarkTheme',"0");
+    }
+
+    // ログ出力
+    console.log("isDarkTheme : " + localStorage.getItem('isDarkTheme'));
+
   });
 
+
+
+  // ダークテーマ時のhover疑似クラスの処理
   // global-nav's hover state 
   $('header .canvas-container .global-nav li a').hover(
     function(){
-      if(isDarkTheme){
+      if(localStorage.getItem('isDarkTheme') == "1"){
         $(this).addClass('dark-theme-white')
       }
     },
@@ -51,7 +81,7 @@ $(function(){
   if(!isJustSwitched){
     $('header .canvas-container .theme-button').hover(
       function(){
-        if(isDarkTheme){
+        if(localStorage.getItem('isDarkTheme') == "1"){
           $(this).addClass('dark-theme-white');
           $('header .canvas-container .theme-button i').addClass('dark-theme-white');
         }
@@ -65,12 +95,10 @@ $(function(){
 
 
 
-
   // HAMBURGER-MENU SWITCH
   $('.trigger-to-switch-hamburger-menu').click(function(){
     $('header .hamburger-menu').fadeToggle();
   });
-
 
 
 
